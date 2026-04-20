123e44ff90fe4d94d0cac31c933f8f79629f967a
JuanPayes <00005923@uca.edu.sv>
2026-04-20 12:22:04 -0600

    Feat: Creando proyecto y entidades del proyecto :tada:

---
+++ Labo1/src/main/java/org/example/labo1/Domain/Entities/Pokemon.java
@@ -0,0 +1,10 @@
+package org.example.labo1.Domain.Entities;
+
+public class Pokemon {
+
+    private String name;
+    private String type;
+    private String weakness;
+    private String zona;
+    private String region;
+}
---
+++ Labo1/src/test/java/org/example/labo1/Labo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.labo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
